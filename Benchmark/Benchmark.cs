8b291bfe7f0dec7860b643580cecd88f57e3155c
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-05-29 04:23:08 +0100

    Tweak iterations to make runtime of each benchmark function similar

--- Benchmark/Benchmark.cs
+++ Benchmark/Benchmark.cs
@@ -18,15 +18,19 @@ namespace Benchmark
         {
             var hashes = new List<HashFunction>
             {
-                new HashFunction("Blake", new Blake()),
-                new HashFunction("CNV0", new CNV0()),
-                new HashFunction("CNV1", new CNV1()),
-                new HashFunction("CNLiteV0", new CNLiteV0()),
-                new HashFunction("CNLiteV1", new CNLiteV1()),
-                new HashFunction("Groestl", new Groestl()),
-                new HashFunction("JH", new JH()),
-                new HashFunction("Keccak", new Keccak()),
-                new HashFunction("Skein", new Skein())
+                new HashFunction("Blake",                           new Blake(),            500000),
+                new HashFunction("CNV0",                            new CNV0(),             10),
+                new HashFunction("CNV0 (Platform Independent)",     new CNV0(false),        10),
+                new HashFunction("CNV1",                            new CNV1(),             10),
+                new HashFunction("CNV1 (Platform Independent)",     new CNV1(false),        10),
+                new HashFunction("CNLiteV0",                        new CNLiteV0(),         20),
+                new HashFunction("CNLiteV0 (Platform Independent)", new CNLiteV0(false),    20),
+                new HashFunction("CNLiteV1",                        new CNLiteV1(),         20),
+                new HashFunction("CNLiteV1 (Platform Independent)", new CNLiteV1(false),    20),
+                new HashFunction("Groestl",                         new Groestl(),          50000),
+                new HashFunction("JH",                              new JH(),               10000),
+                new HashFunction("Keccak",                          new Keccak(),           300000),
+                new HashFunction("Skein",                           new Skein(),            500000)
             };
 
             while (true)
@@ -42,7 +46,12 @@ namespace Benchmark
                     index++;
                 }
 
+                Console.WriteLine($"{index}. Benchmark ALL");
+
+                index++;
+
                 Console.WriteLine($"{index}. Exit");
+
                 Console.Write("Enter your selection: ");
 
                 // Get selection
@@ -55,8 +64,17 @@ namespace Benchmark
                     {
                         HashBenchmark(hashes[selectionNum - 1]);
                     }
-                    // Exit
+                    /* Test all */
                     else if (selectionNum == hashes.Count + 1)
+                    {
+                        foreach (var hashFunction in hashes)
+                        {
+                            HashBenchmark(hashFunction);
+                            Console.WriteLine();
+                        }
+                    }
+                    /* Exit */
+                    else if (selectionNum == hashes.Count + 2)
                     {
                         Console.WriteLine("Bye.");
                         return;
@@ -70,15 +88,17 @@ namespace Benchmark
                 {
                     Console.WriteLine("Invalid input...");
                 }
+
+                Console.WriteLine();
             }
         }
 
-        public static void HashBenchmark(HashFunction hash, int iterations = 100)
+        public static void HashBenchmark(HashFunction hash)
         { 
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            for (int i = 0; i < iterations; i++)
+            for (int i = 0; i < hash.Iterations; i++)
             {
                 byte[] input = SecureRandom.Bytes(64);
                 hash.Function.Hash(input);
@@ -93,22 +113,25 @@ namespace Benchmark
                 ts.Hours, ts.Minutes, ts.Seconds,
                 ts.Milliseconds / 10);
 
-            Console.WriteLine($"Ran {iterations} iterations of {hash.Name}:");
+            Console.WriteLine($"Ran {hash.Iterations} iterations of {hash.Name}:");
             Console.WriteLine($"Runtime: {elapsedTime}");
-            Console.WriteLine($"Hashes per second: {iterations / ts.TotalSeconds}");
+            Console.WriteLine($"Hashes per second: {hash.Iterations / ts.TotalSeconds:F2}");
         }
     }
 
     public class HashFunction
     {
-        public HashFunction(string name, IHashProvider hashFunction)
+        public HashFunction(string name, IHashProvider hashFunction, int iterations)
         {
             this.Name = name;
             this.Function = hashFunction;
+            this.Iterations = iterations;
         }
 
         public string Name { get; }
 
         public IHashProvider Function { get; }
+
+        public int Iterations { get; }
     }
 }
