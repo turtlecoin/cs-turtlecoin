aace2f1d996b3834ece686f4a685fd0c39dd0ff7
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-06-01 01:23:49 +0100

    Add tests to match C++ implementation hashes

--- Tests/HashTests/CryptoNightTests.cs
+++ Tests/HashTests/CryptoNightTests.cs
@@ -12,6 +12,11 @@ namespace Tests
     [TestClass]
     public class CryptoNightTests
     {
+        /* This is hex data in the C++ code, wheras our other inputs are ascii -
+           So convert to ascii so the hash is as expected rather than being
+           double hexified. */
+        private static string INPUT_DATA = Encoding.HexStringToString("0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02");
+
         [TestMethod]
         public void TestCNV0()
         {
@@ -20,7 +25,8 @@ namespace Tests
                 { "", "eb14e8a833fac6fe9a43b57b336789c46ffe93f2868452240720607b14387e11" },
                 { "The quick brown fox jumps over the lazy dog", "3ebb7f9f7d273d7c318d869477550cc800cfb11b0cadb7ffbdf6f89f3a471c59" },
                 { "The quick brown fox jumps over the lazy dog.", "e37cc1b6fabcd3652b6d2879ac806e39f591f9d1c20be0c6b99cf6bef31158a2" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "d986f765ad299c605eba4712ffe11918ed9f39c4358949fd11a2cfd3f04fab35" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "d986f765ad299c605eba4712ffe11918ed9f39c4358949fd11a2cfd3f04fab35" },
+                { INPUT_DATA, "1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f" },
             };
 
             HashTests.Test(testVectors, new CNV0());
@@ -34,7 +40,8 @@ namespace Tests
                 { "", "eb14e8a833fac6fe9a43b57b336789c46ffe93f2868452240720607b14387e11" },
                 { "The quick brown fox jumps over the lazy dog", "3ebb7f9f7d273d7c318d869477550cc800cfb11b0cadb7ffbdf6f89f3a471c59" },
                 { "The quick brown fox jumps over the lazy dog.", "e37cc1b6fabcd3652b6d2879ac806e39f591f9d1c20be0c6b99cf6bef31158a2" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "d986f765ad299c605eba4712ffe11918ed9f39c4358949fd11a2cfd3f04fab35" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "d986f765ad299c605eba4712ffe11918ed9f39c4358949fd11a2cfd3f04fab35" },
+                { INPUT_DATA, "1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f" },
             };
 
             HashTests.Test(testVectors, new CNV0(false));
@@ -47,7 +54,8 @@ namespace Tests
             {
                 { "The quick brown fox jumps over the lazy dog", "94f5dec524fad6d32004c55c035e5ea223e7315be20e2dc5b8a0ac7464ffeb1f" },
                 { "The quick brown fox jumps over the lazy dog.", "86d34efc73e709dcc0f862725be692d1f8c5b407b4d730cd309acf80cc8f7c73" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "246e1f4e7a61e0b29d4fefe33bb48b175468d28c0e44e84cb0cf244be8af9a12" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "246e1f4e7a61e0b29d4fefe33bb48b175468d28c0e44e84cb0cf244be8af9a12" },
+                { INPUT_DATA, "c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122" },
             };
 
             HashTests.Test(testVectors, new CNV1());
@@ -60,7 +68,8 @@ namespace Tests
             {
                 { "The quick brown fox jumps over the lazy dog", "94f5dec524fad6d32004c55c035e5ea223e7315be20e2dc5b8a0ac7464ffeb1f" },
                 { "The quick brown fox jumps over the lazy dog.", "86d34efc73e709dcc0f862725be692d1f8c5b407b4d730cd309acf80cc8f7c73" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "246e1f4e7a61e0b29d4fefe33bb48b175468d28c0e44e84cb0cf244be8af9a12" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "246e1f4e7a61e0b29d4fefe33bb48b175468d28c0e44e84cb0cf244be8af9a12" },
+                { INPUT_DATA, "c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122" },
             };
 
             HashTests.Test(testVectors, new CNV1(false));
@@ -86,7 +95,8 @@ namespace Tests
                 { "", "4cec4a947f670ffdd591f89cdb56ba066c31cd093d1d4d7ce15d33704c090611" },
                 { "The quick brown fox jumps over the lazy dog", "fbbbc024c37acff2e7302275458447f888a8d6361ce407c391be72ed34c16fee" },
                 { "The quick brown fox jumps over the lazy dog.", "6d0e743fca6358bd0e9b365a68887fa9abf9f2940fe50682be28759b776d0fb0" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "38c887c08c2c9398d411c4bbf9b3eb707087906a5326ebeb3b238e7867fb1a9b" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "38c887c08c2c9398d411c4bbf9b3eb707087906a5326ebeb3b238e7867fb1a9b" },
+                { INPUT_DATA, "28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd" },
             };
 
             HashTests.Test(testVectors, new CNLiteV0());
@@ -100,7 +110,8 @@ namespace Tests
                 { "", "4cec4a947f670ffdd591f89cdb56ba066c31cd093d1d4d7ce15d33704c090611" },
                 { "The quick brown fox jumps over the lazy dog", "fbbbc024c37acff2e7302275458447f888a8d6361ce407c391be72ed34c16fee" },
                 { "The quick brown fox jumps over the lazy dog.", "6d0e743fca6358bd0e9b365a68887fa9abf9f2940fe50682be28759b776d0fb0" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "38c887c08c2c9398d411c4bbf9b3eb707087906a5326ebeb3b238e7867fb1a9b" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "38c887c08c2c9398d411c4bbf9b3eb707087906a5326ebeb3b238e7867fb1a9b" },
+                { INPUT_DATA, "28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd" },
             };
 
             HashTests.Test(testVectors, new CNLiteV0(false));
@@ -113,7 +124,8 @@ namespace Tests
             {
                 { "The quick brown fox jumps over the lazy dog", "973a324237703e0f2ebe678a0000a00afb14c2c394c1859c84bcaa7b90bc56db" },
                 { "The quick brown fox jumps over the lazy dog.", "b860f59d6aef32c7cacac02c4ac794066402dbd30c64c7fb733600a91441326d" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "f7a5217873b802940a629573f6a100deb25764af254f8f4fef1a8b9d51ef3cc5" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "f7a5217873b802940a629573f6a100deb25764af254f8f4fef1a8b9d51ef3cc5" },
+                { INPUT_DATA, "87c4e570653eb4c2b42b7a0d546559452dfab573b82ec52f152b7ff98e79446f" },
             };
 
             HashTests.Test(testVectors, new CNLiteV1());
@@ -126,7 +138,8 @@ namespace Tests
             {
                 { "The quick brown fox jumps over the lazy dog", "973a324237703e0f2ebe678a0000a00afb14c2c394c1859c84bcaa7b90bc56db" },
                 { "The quick brown fox jumps over the lazy dog.", "b860f59d6aef32c7cacac02c4ac794066402dbd30c64c7fb733600a91441326d" },
-                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "f7a5217873b802940a629573f6a100deb25764af254f8f4fef1a8b9d51ef3cc5" }
+                { "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself", "f7a5217873b802940a629573f6a100deb25764af254f8f4fef1a8b9d51ef3cc5" },
+                { INPUT_DATA, "87c4e570653eb4c2b42b7a0d546559452dfab573b82ec52f152b7ff98e79446f" },
             };
 
             HashTests.Test(testVectors, new CNLiteV1(false));
